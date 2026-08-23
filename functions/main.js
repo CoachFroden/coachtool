bee76d38f51fc3f9b3ca16bd9ed946b8d6339aa7
CoachFroden <frode@vibliz.com>
2026-08-23 23:24:40 +0200

    Fjern motstandersjekk fra Cloud Functions

--- functions/main.js
+++ functions/main.js
@@ -1,9 +1,5 @@
 const legacyFunctions = require("./index");
-const opponentAnalysisFunctions = require("./opponent-analysis");
-const opponentPostAnalysisFunctions = require("./opponent-post-analysis");
 
 module.exports = {
-  ...legacyFunctions,
-  ...opponentAnalysisFunctions,
-  ...opponentPostAnalysisFunctions
+  ...legacyFunctions
 };
