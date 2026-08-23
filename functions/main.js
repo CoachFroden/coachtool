9058ac920e49bd653cc1a0bab56ad36a43002813
CoachFroden <frode@vibliz.com>
2026-08-23 18:57:04 +0200

    Legg til ettersjekk av motstander etter kamp
    * Legg til ettersjekk på spilte kamper
    
    * Legg til backend for ettersjekk av motstander
    
    * Registrer ettersjekk-funksjonen
    
    * Last inn ettersjekk på oversiktssiden

--- functions/main.js
+++ functions/main.js
@@ -1,7 +1,9 @@
 const legacyFunctions = require("./index");
 const opponentAnalysisFunctions = require("./opponent-analysis");
+const opponentPostAnalysisFunctions = require("./opponent-post-analysis");
 
 module.exports = {
   ...legacyFunctions,
-  ...opponentAnalysisFunctions
+  ...opponentAnalysisFunctions,
+  ...opponentPostAnalysisFunctions
 };
